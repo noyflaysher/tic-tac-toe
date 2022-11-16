047a4d14d3e40bcc46c0f1f3b16259a3515653d9
noyflaysher <noyflaysher12@gmail.com>
2022-11-16 21:42:13 +0200

    last changes

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-const container=document.getElementById("container");
-const result=document.getElementById("result");
-const restart=document.getElementById("newGame");
+const container=document.querySelector(".container");
+const result=document.querySelector(".result");
+const restart=document.querySelector(".newGame");
 let boxes;
-let turnX=true;
-let correctCombination;
+let turn="X";
+let theWinCombination;
 const winCombinations=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+restart.addEventListener("click",()=>restartGame());
 
-newGame("");
-restart.addEventListener("click",()=>newGame("restart"));
+init();
 
-function newGame(type){
+
+function init(){
+    for(let i=0;i<9;i++){
+        box=document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+        box.addEventListener("click", clickBox,{once:true});
+    }
+}
+
+function restartGame(){
     let box;
     for(let i=0;i<9;i++){
-        if(type==="restart"){
-            boxes[i].innerText="";
-            result.innerText="";
-            container.style.pointerEvents="unset";
-            boxes[i].removeEventListener("click",clickBox);
-            turnX=true;
-            restart.classList.remove("showNewGame");
-            correctCombination.every((item)=> boxes[item].style.background="none");
-        }else{
-            box=document.createElement("div");
-            box.classList.add("box");
-            container.appendChild(box);
-        }
-        
-        (box||boxes[i]).addEventListener("click", clickBox,{once:true});
+        boxes[i].innerText="";
+        result.innerText="";
+        container.style.pointerEvents="unset";
+        boxes[i].removeEventListener("click",clickBox);
+        turn="X";
+        restart.classList.remove("showNewGame");
+        theWinCombination.every((item)=> boxes[item].style.background="none");
+        boxes[i].addEventListener("click", clickBox,{once:true});
     }
-    
 }
 
 boxes=document.querySelectorAll(".box");
 
 function clickBox(e){
     const currentBox=e.target;
-    if(turnX){
-        turnX=false;
+    if(turn==="X"){
+        turn="Y";
         currentBox.innerText="X";
     }
     else{
-        turnX=true;
+        turn="X";
         currentBox.innerText="O";
     }
 
@@ -49,23 +51,23 @@ function clickBox(e){
     const win=checkWin(currentSelection);
 
     if(win){
-        correctCombination.every((item)=> boxes[item].style.background="blue");
+        theWinCombination.every((item)=> boxes[item].style.background="blue");
         gameOver("win",currentSelection);
     }
-    else if(finishGame()){
-        gameOver("draw", currentSelection);
+    else if(turnsOver()){
+        gameOver("turns over", currentSelection);
     }
 }
 
 function checkWin(currentSelection){
     return winCombinations.some((combination)=>{
-        correctCombination=combination;
+        theWinCombination=combination;
         return combination.every((item)=> boxes[item].innerText === currentSelection)
     })
 
 }
 
-function finishGame(){
+function turnsOver(){
     return [...boxes].every((box)=>box.innerText==="X" || box.innerText==="O");
 }
 
